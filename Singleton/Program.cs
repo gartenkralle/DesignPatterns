d07effd2503868827e4a49ac231fb6d7f9591f4a
Clemens Schneider <clemens.schneider@posteo.de>
2019-01-23 12:41:54 +0100

    create

---
+++ Singleton/Program.cs
@@ -0,0 +1,85 @@
+﻿namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EagerLoading.Singleton s1 = EagerLoading.Singleton.Instance;
+            EagerLoading.Singleton s2 = EagerLoading.Singleton.Instance;
+
+            if (s1 == s2)
+                System.Console.WriteLine("Equals");
+            else
+                System.Console.WriteLine("Not equals");
+        }
+    }
+}
+
+namespace EagerLoading
+{
+    /* Singleton */
+
+    // Eager loading
+    // Prefer if the singleton object is needed in any case
+    sealed class Singleton
+    {
+        private Singleton() { }
+
+        public static Singleton Instance { get; } = new Singleton();
+    }
+}
+
+namespace LazyLoadingWithLock
+{
+    // Lazy loading (classic way)
+    // Prefer if the singleton object is not needed in any cae
+    sealed class Singleton
+    {
+        private Singleton() { }
+        private static volatile Singleton instance;
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (instance == null) // this line is only for performance improvement
+                {
+                    lock (_lock)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new Singleton();
+                        }
+                    }
+                }
+                return instance;
+            }
+        }
+
+        private static object _lock = new object();
+    }
+}
+
+namespace LazyLoadingWithLazyClass
+{
+    // Lazy loading (with Lazy class)
+    using System;
+
+    sealed class Singleton
+    {
+        private Singleton() { }
+        private static readonly Lazy<Singleton> instance = new Lazy<Singleton>(() => new Singleton());
+
+        public static Singleton Instance
+        {
+            get
+            {
+                return instance.Value;
+            }
+        }
+    }
+}
+
+/* Singleton vs. Static class */
+
+// A singleton can inherit from interfaces or a base class, a static class cannot
