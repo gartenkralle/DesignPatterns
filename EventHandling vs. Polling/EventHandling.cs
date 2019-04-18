d53282fa5258e80ecd4b33ab2d603883b422bf16
Clemens Schneider <clemens.schneider@posteo.de>
2019-04-18 16:45:02 +0200

    Update EventHandling.cs

--- EventHandling vs. Polling/EventHandling.cs
+++ EventHandling vs. Polling/EventHandling.cs
@@ -1,1 +1,51 @@
+using System;
+using System.ComponentModel;
 
+namespace EventHandling
+{
+    class Program
+    {
+        static Database database;
+
+        static void Main(string[] args)
+        {
+            database = new Database();
+
+            database.Temperature = 10;
+            database.TemperatureChanged += Database_TemperatureChanged;
+            database.Temperature = 20;
+            database.Temperature = 30;
+        }
+
+        private static void Database_TemperatureChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(Database.Temperature))
+            {
+                Console.WriteLine("New Temperature is: " + (int)sender);
+            }
+        }
+    }
+
+    public class Database
+    {
+        public event PropertyChangedEventHandler TemperatureChanged;
+
+        private int temperature;
+
+        public int Temperature
+        {
+            get
+            {
+                return temperature;
+            }
+            set
+            {
+                if (value != temperature)
+                {
+                    temperature = value;
+                    TemperatureChanged?.Invoke(temperature, new PropertyChangedEventArgs(nameof(Temperature)));
+                }
+            }
+        }
+    }
+}
